e998139d13c1a360a1fb7d3eb4bf432bc834e0db
Monica Powell <monica@aboutmonica.com>
2020-05-11 14:10:06 -0400

    remove unecessary SingleLibraryPage

--- src/components/SingleLibraryPage.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { IS_MULTI_LIBRARY } from "../utils/env";
-import ErrorPage from "../pages/_error";
-
-const SingleLibraryPage: React.FC<{}> = ({ children }) => {
-  return !IS_MULTI_LIBRARY ? <>{children}</> : <ErrorPage statusCode={404} />;
-};
-
-export default SingleLibraryPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,13 @@
 import { NextPage } from "next";
 import Collection from "../components/Collection";
 
-import SingleLibraryPage from "../components/SingleLibraryPage";
 import Layout from "../components/Layout";
 
 const LibraryHome: NextPage = () => {
   return (
-    <SingleLibraryPage>
-      <Layout>
-        <Collection />
-      </Layout>
-    </SingleLibraryPage>
+    <Layout>
+      <Collection />
+    </Layout>
   );
 };
 
