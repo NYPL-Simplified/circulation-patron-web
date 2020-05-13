844e00f0baf998b0a584efff029885c35fa1b7b0
Monica Powell <monica@aboutmonica.com>
2020-05-12 21:01:57 -0400

    re-add showFormatFilter on index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import Layout from "../components/Layout";
 
 const LibraryHome: NextPage = () => {
   return (
-    <Layout>
+    <Layout showFormatFilter>
       <Collection />
     </Layout>
   );
