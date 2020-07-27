6ea5fafb2dabc9a6c15b62f148d7f504403ffe76
Kristo Jorgenson <kristo.jorgenson@gmail.com>
2020-07-27 14:33:12 +0200

    lint

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,11 +1,7 @@
 /* eslint-disable camelcase */
 import { State } from "opds-web-client/lib/state";
 import { CollectionState } from "opds-web-client/lib/reducers/collection";
-import {
-  CollectionData,
-  BookData,
-  AuthMethod
-} from "opds-web-client/lib/interfaces";
+import { CollectionData, BookData } from "opds-web-client/lib/interfaces";
 
 export interface PathFor {
   (collectionUrl?: string, bookUrl?: string): string;
