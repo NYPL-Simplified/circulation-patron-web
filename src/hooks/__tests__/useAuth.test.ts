4c10e01c4381210477b747eaef871c780ac3bcfa
Monica Powell <monica@aboutmonica.com>
2020-08-11 19:04:07 -0400

    add test for when CleverAuthPlugin.lookForCredentials doesn't return credentials

--- src/hooks/__tests__/useAuth.test.ts
+++ src/hooks/__tests__/useAuth.test.ts
@@ -1,13 +1,16 @@
 import { getAccessToken } from "../useAuth";
+import CleverAuthPlugin from "../../auth/cleverAuthPlugin";
+
 describe("getAccessToken", () => {
   const { location } = window;
 
-  beforeAll(() => {
+  beforeEach(() => {
     delete window.location;
   });
 
-  afterAll(() => {
+  afterEach(() => {
     window.location = location;
+    jest.restoreAllMocks();
   });
 
   const mockSAMLToken = "potatokey";
@@ -22,6 +25,22 @@ describe("getAccessToken", () => {
   test("returns TOKEN_NOT_FOUND without a token when there is no access_token in query or access_token in window.location.hash", async () => {
     expect(getAccessToken(mockRouterWithEmptyQuery)).toStrictEqual({
       token: { credentials: {} },
+      type: "TOKEN_NOT_FOUND"
+    });
+  });
+
+  test("returns TOKEN_NOT_FOUND when CleverAuthPlugin.lookForCredentials() returns an error without credentials", async () => {
+    window.location = { hash: "#access_token=fry" } as any;
+
+    jest
+      .spyOn(CleverAuthPlugin, "lookForCredentials")
+      .mockImplementation(() => {
+        "Oops something went wrong";
+      });
+    expect(getAccessToken(mockRouterWithEmptyQuery)).toStrictEqual({
+      token: {
+        credentials: {}
+      },
       type: "TOKEN_NOT_FOUND"
     });
   });
