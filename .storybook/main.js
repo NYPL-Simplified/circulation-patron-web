b48c51dff99e4debdc8404669442a1863767d20a
kristojorg <kristo.jorgenson@gmail.com>
2020-10-20 22:50:20 +0200

    setup storybook

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  webpackFinal: async config => {
+    config.resolve.alias['core-js/modules'] = '@storybook/core/node_modules/core-js/modules';
+    config.resolve.alias['core-js/features'] = '@storybook/core/node_modules/core-js/features';
+
+    return config
+  }
+}
