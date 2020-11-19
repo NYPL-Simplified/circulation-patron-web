90c1999369b42511d908b813a7418938957c609f
kristojorg <kristo.jorgenson@gmail.com>
2020-11-19 13:22:22 +0000

    put auth method in url

---
+++ src/pages/[library]/login/index.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { NextPage, GetStaticPaths, GetStaticProps } from "next";
+import LayoutPage from "components/LayoutPage";
+import withAppProps, { AppProps } from "dataflow/withAppProps";
+import Login from "auth/Login";
+
+const LoginPage: NextPage<AppProps> = ({ library, error }) => {
+  return (
+    <LayoutPage library={library} error={error}>
+      <Login />
+    </LayoutPage>
+  );
+};
+
+export const getStaticProps: GetStaticProps = withAppProps();
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true
+  };
+};
+
+export default LoginPage;
