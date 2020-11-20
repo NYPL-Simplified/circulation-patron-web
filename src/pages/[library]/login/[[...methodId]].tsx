0e46fcfee865ade660b701ecb8f43c0ef56a6a2b
kristojorg <kristo.jorgenson@gmail.com>
2020-11-20 19:51:36 +0000

    unused vars

--- src/pages/[library]/login/[[...methodId]].tsx
+++ src/pages/[library]/login/[[...methodId]].tsx
@@ -2,23 +2,7 @@ import * as React from "react";
 import { NextPage, GetStaticPaths, GetStaticProps } from "next";
 import LayoutPage from "components/LayoutPage";
 import withAppProps, { AppProps } from "dataflow/withAppProps";
-import { useRouter } from "next/router";
-import extractParam from "dataflow/utils";
-import useLibraryContext from "components/context/LibraryContext";
-import { OPDS1 } from "interfaces";
-// import BasicAuthForm from "auth/BasicAuthHandler";
-import CleverAuthHandler from "auth/CleverAuthHandler";
-import SamlAuthHandler from "auth/SamlAuthHandler";
-import Login from 'auth/Login';
-
-/**
- * - Render selector: none, buttons, or combobox
- * Our states are
- *  - No method selected
- *    - Render the selector
- *  - Method selected
- *    - Render the 
- */
+import Login from "auth/Login";
 
 const LoginPage: NextPage<AppProps> = ({ library, error }) => {
   return (
@@ -27,26 +11,6 @@ const LoginPage: NextPage<AppProps> = ({ library, error }) => {
     </LayoutPage>
   );
 };
-
-// const LoginComponent = () => {
-//   const router = useRouter();
-//   const methodId = extractParam(router.query, "methodId");
-//   const { authMethods } = useLibraryContext();
-
-//   const selectedMethod = authMethods.find(m => m.id === methodId);
-
-//   // TODO: if there is somehow no selected method, return to main login
-//   if (!selectedMethod) return null;
-
-//   switch (selectedMethod.type) {
-//     case OPDS1.BasicAuthType:
-//       return <BasicAuthForm method={selectedMethod} />;
-//     case OPDS1.SamlAuthType:
-//       return <SamlAuthHandler method={selectedMethod} />;
-//     case OPDS1.CleverAuthType:
-//       return <CleverAuthHandler method={selectedMethod} />;
-//   }
-// };
 
 export const getStaticProps: GetStaticProps = withAppProps();
 
