1124b482476c575b74893f547a36839d0821448f
kristojorg <kristo.jorgenson@gmail.com>
2020-11-20 18:48:32 +0000

    redirect clever auth properly

--- src/auth/CleverAuthHandler.tsx
+++ src/auth/CleverAuthHandler.tsx
@@ -7,23 +7,28 @@ import Stack from "components/Stack";
 import { useRouter } from "next/router";
 import useUser from "components/context/UserContext";
 import { clientOnly } from "components/ClientOnly";
+import extractParam from "dataflow/utils";
+import useLinkUtils from "hooks/useLinkUtils";
 
 const CleverAuthHandler: React.FC<{ method: OPDS1.CleverAuthMethod }> = ({
   method
 }) => {
-  // if (typeof window === "undefined") return null;
-  const { push } = useRouter();
+  const { push, query } = useRouter();
   const { token } = useUser();
+  const { buildMultiLibraryLink } = useLinkUtils();
 
-  const currentUrl = window.location.origin + window.location.pathname;
+  const catalogRootUrl = buildMultiLibraryLink("/");
+  const nextUrl = extractParam(query, "nextUrl");
+  const redirectUrl = `${window.location.origin}${nextUrl ?? catalogRootUrl}`;
 
   const authenticateHref = method.links?.find(
     link => link.rel === "authenticate"
   )?.href;
+
   // double encoding is required for unshortened book urls to be redirected to properly
   const authUrl = authenticateHref
     ? `${authenticateHref}&redirect_uri=${encodeURIComponent(
-        encodeURIComponent(currentUrl)
+        encodeURIComponent(redirectUrl)
       )}`
     : undefined;
 
