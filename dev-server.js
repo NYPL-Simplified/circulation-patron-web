e904382daf4ed9735e60baed73abb2456e184c6d
Michael Kaplan <MKaplan@whereoware.com>
2021-01-12 14:19:25 -0500

    [SIMPLY-3331] Update dev server settings to allow site to be accessed via ip address, which was necessary in order to test on iOS devices using browserstack

--- dev-server.js
+++ dev-server.js
@@ -6,18 +6,31 @@ const fs = require("fs");
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
+const port = 3000;
 
 const httpsOptions = {
   key: fs.readFileSync("./localhost.key"),
   cert: fs.readFileSync("./localhost.crt")
 };
 
+var test = Object.values(require("os").networkInterfaces()).reduce(
+  (r, list) =>
+    r.concat(
+      list.reduce(
+        (rr, i) =>
+          rr.concat((i.family === "IPv4" && !i.internal && i.address) || []),
+        []
+      )
+    ),
+  []
+);
+
 app.prepare().then(() => {
   createServer(httpsOptions, (req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(3000, err => {
+  }).listen(port, "0.0.0.0", err => {
     if (err) throw err;
-    console.log("> Ready on https://localhost:3000");
+    console.log(`> Ready on https://localhost:${port} and ${test}:${port}`);
   });
 });
